dfdc5159f2bcf1cc012f0a82ad432556e5a896eb
Daigo Hirooka <daigo.hirooka@gmail.com>
2020-08-30 05:27:24 +0900

    remove unused import statement #2

--- tests/layers/test_extractor.py
+++ tests/layers/test_extractor.py
@@ -1,13 +1,11 @@
 import pytest
 import numpy as np
 import tensorflow as tf
-import torch
 
 
 SEED = 1
 np.random.seed(SEED)
 tf.random.set_seed(SEED)
-torch.manual_seed(SEED)
 IMAGE_SIZE = (128, 128)
 NUM_CHANNELS = 32
 FILTERS = 64
